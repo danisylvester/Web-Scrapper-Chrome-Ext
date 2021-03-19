bff77a450e574c31326d052aff06ad2e64cff05b
danisylvester <maria.sylvester10@gmail.com>
2021-03-19 18:09:23 -0500

    Added listener to grab translation array from content component. Created download function that also parses array.

--- popup.js
+++ popup.js
@@ -1,8 +1,59 @@
-let translateBtn = document.getElementById("translate");
+let elements = [];
 
-translateBtn.addEventListener("click", async () => {
-    const clicked = true;
-    let tab = await chrome.tabs.query({ active: true, currentWindow: true });
+// Listening for message from content file
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
 
-  chrome.tab.sendMessage(tab.id, {clicked})
+  for (let i = 0; i < request.length; i++) {
+    elements.push({
+      key: request[i].key,
+      value: request[i].value,
+    });
+    console.log(elements[i]);
+  }
+  sendResponse(request);
 });
+
+// Converting elements array to text and downloading text file to user's computer
+function downloadTranslations() {
+  console.log(`elements check ${elements}`);
+
+  let result = JSON.stringify(elements, null, 1);
+  let formatResult = result
+    .replace("[", "")
+    .replace(/{/g, "")
+    .replace(/}/g, "")
+    .replace(/"/g, "")
+    .replace("]", "")
+    .replace(/,/g, "")
+    .replace(/key/g, "Language")
+    .replace(/value/g, "Translation");
+
+  console.log(formatResult);
+
+  let blob = new Blob([formatResult], { type: "text/plain" });
+
+  console.log(blob);
+
+  let url = URL.createObjectURL(blob);
+
+  chrome.downloads.download({
+    url: url,
+  });
+}
+
+window.onload = function () {
+  document.getElementById("download").onclick = downloadTranslations;
+
+  chrome.windows.getCurrent(function (currentWindow) {
+    chrome.tabs.query(
+      { active: true, windowId: currentWindow.id },
+      function (activeTabs) {
+        chrome.tabs.executeScript(activeTabs[0].id, {
+          file: "content.js",
+          allFrames: true,
+        });
+      }
+    );
+  });
+};
