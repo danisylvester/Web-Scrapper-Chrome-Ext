c03bcc0e7b57055dcab9cc08cd7c6ea33d69f359
danisylvester <maria.sylvester10@gmail.com>
2021-03-22 22:06:48 -0500

    Pulled title from website in order to name our download file.

--- content.js
+++ content.js
@@ -1,14 +1,25 @@
-let elements = document.getElementsByClassName("translation");
-let labels = document.getElementsByTagName("label");
+// Grabbing input value for our download title and trimming if more than 5 words
+var titleArr = document.getElementById("focusOnMe").value.split(' ');
+if(titleArr.length > 5){
+  var title = titleArr.slice(0,5).join().replace(/,/g,' ');
+  title += '...'
+} else{
+  var title = titleArr.join().replace(/,/g, ' ');
+}
+console.log(title);
 
-let translationValues = [];
-let translationLabels = [];
-let translations = [];
+// Grabbing language labels and translations values from webpage
+var elements = document.getElementsByClassName("translation");
+var labels = document.getElementsByTagName("label");
+// Declaring temp label and value arrays and final translation arr
+var translationLabels = [];
+var translationValues = [];
+var translations = [{key: 'Original', value: title}];
 
 // Getting label value
 for (let i = 0; i < labels.length; i++) {
   let labelEl = labels[i].innerText;
-  let end = labelEl.length - 17;
+  let end = labelEl.length - 17; 
   let label = labelEl.substring(0, end);
 
   translationLabels.push(label);
@@ -21,7 +32,7 @@ for (let i = 0; i < elements.length; i++) {
 }
 
 // Creating array of objects with translation label and value
-for (let i = 0; i < translationLabels.length; i++) {
+for (let i = 0; i <= translationLabels.length; i++) {
   translations.push({
     key: translationLabels[i],
     value: translationValues[i],
